653a3132e54de2d57c676f706f3a352a6bc6d23e
XpressRazor <xpressrazor@gmail.com>
2013-11-03 16:54:13 +0545

    Some installation files

---
+++ installation/all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install ubuntu-restricted-extras vlc artha chromium-browser audacity clementine cclive youtube-dl cryptkeeper dosbox smplayer feh irssi ncmpcpp compizconfig-settings-manager bum easytag gimp meld openshot ghex pavucontrol mupen64plus soundconverter ffmpeg aria2 uget samba smbclient nautilus-share fbreader gufw xbacklight scribes vim nautilus-open-terminal p7zip-full tmux emacs gksu pcmanfm i3 keepassx mesa-utils qbittorrent nmap 
+
