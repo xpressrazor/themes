37365d45bbc6c82d002601e598601cf9c8ebab85
XpressRazor <xpressrazor@gmail.com>
2013-11-05 09:05:28 +0545

    Added ssh, git tips, xsel help etc

--- installation/all.sh
+++ installation/all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install ubuntu-restricted-extras vlc artha chromium-browser audacity clementine cclive cryptkeeper dosbox smplayer feh irssi ncmpcpp compizconfig-settings-manager bum gimp meld openshot ghex pavucontrol mupen64plus soundconverter ffmpeg aria2 uget samba smbclient nautilus-share fbreader gufw xbacklight vim nautilus-open-terminal tmux emacs gksu pcmanfm i3 keepassx mesa-utils qbittorrent nmap 
+sudo apt-get install ubuntu-restricted-extras vlc artha chromium-browser audacity clementine cclive cryptkeeper dosbox smplayer feh irssi ncmpcpp compizconfig-settings-manager bum gimp meld openshot ghex pavucontrol mupen64plus soundconverter ffmpeg aria2 uget samba smbclient nautilus-share fbreader gufw xbacklight vim nautilus-open-terminal tmux emacs gksu pcmanfm i3 keepassx mesa-utils qbittorrent nmap xclip
 
